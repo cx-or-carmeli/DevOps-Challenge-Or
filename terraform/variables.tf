432dbfcaf8a97f0d788cf1e5ed14b0d4077de53d
cx-or-carmeli <or.carmeli@checkmarx.com>
2025-04-17 17:46:28 +0300

    Changes in main.tf + variables file

---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+  variable "grafana_auth" {
+    description = "Grafana authentication string (username:password)"
+    type        = string
+    sensitive   = true
+  }
+
+  variable "postgres_password" {
+    description = "PostgreSQL password"
+    type        = string
+    default     = "postgres"
+  }
